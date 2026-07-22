fa4406cf093d535021bb9550f0b8d8111978c492
Divya Kumari <divyakumaridk05@gmail.com>
2026-07-22 21:50:19 +0530

    Implement lowest common ancestor for binary tree

---
+++ Tree/LowerCommonAncestor(236).java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean getpath(TreeNode root,TreeNode n,List<TreeNode> path){
+        if(root==null) return false; // no value is there
+        path.add(root); // add root in the path
+        if(root==n) return true; // got expected value
+        if(getpath(root.left,n,path)||getpath(root.right,n,path)) return true; //return early if found in left subtree 
+        path.remove(path.size()-1); // backtrack
+        return false; // if not found
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> path1 = new ArrayList<>();
+        List<TreeNode> path2 = new ArrayList<>();
+        getpath(root,p,path1);
+        getpath(root,q,path2);
+        int i=0;
+        for(;i<path1.size()&&i<path2.size();i++){
+            if(path1.get(i) != path2.get(i)) break;
+        }
+        TreeNode lowestCommonAncestor = path1.get(i-1);
+        return lowestCommonAncestor;
+    }
+}
