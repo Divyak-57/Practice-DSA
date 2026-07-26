556b4333cd184c46f0ac20a26759e67d09b72dfd
Divya Kumari <divyakumaridk05@gmail.com>
2026-07-26 10:45:01 +0530

    Add Fibonacci.java with dynamic programming solution
    Implement Fibonacci sequence calculation using dynamic programming.

---
+++ DP/Fibonacci.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+class Main {
+    public static int solve(int n){
+        if(n<0) return 0;
+        if (n == 0) return 1;
+        if (n == 1) return 1; 
+        int[] dp = new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(solve(n));
+    }
+}
+       
