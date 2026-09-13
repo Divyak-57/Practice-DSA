b2251a6f99ae914f19a99c205147debc49d629a8
Divya Kumari <divyakumaridk05@gmail.com>
2026-09-13 10:13:00 +0530

    Implement DFS to count provinces in a graph

---
+++ Graph/Number of Provinces(547).java
@@ -0,0 +1,24 @@
+class Solution {
+    private void dfs(int i, int[][] isConnected, boolean[] vis) {
+        vis[i] = true;
+        for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[i][j] == 1 && !vis[j]) {
+                dfs(j, isConnected, vis);
+            }
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] vis = new boolean[n];
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                dfs(i, isConnected, vis);
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
